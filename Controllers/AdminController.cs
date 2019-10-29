dbfcac02b04b1f187affe1c2ae35d4d133743fcf
DarkAce01 <quadrikazeem01@gmail.com>
2019-10-29 15:52:01 +0100

    admin move

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -72,6 +72,76 @@ namespace SportEquipWeb.Controllers
             return RedirectToAction("EquipmentList");
         }
 
+        
+        public ActionResult AllUsers()
+        {
+            var allUsers = (from s in db.Users
+                            select s).Where(u => u.UserName != "admin@gmail.com").ToList();
 
+
+            return View(allUsers.ToList());
+        }
+
+        public ActionResult UnlockUser(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ApplicationUser applicationUser = db.Users.Find(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            applicationUser.IsEnabled = true;
+            db.Entry(applicationUser).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("AllUsers");
+        }
+        public ActionResult LockUser(string id)
+        {
+
+            if (String.IsNullOrEmpty(id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = db.Users.Find(id);
+
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+        
+        public ActionResult LockOutConfirmed(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ApplicationUser applicationUser = db.Users.Find(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            applicationUser.IsEnabled = false;
+            db.Entry(applicationUser).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("AllUsers");
+
+        }
+        
+        public ActionResult AllTransactions()
+        {
+            var transaction = (from s in db.Transactions
+                               select s);
+            var allTransaction = transaction.Include(e => e.Equipment).Include(u => u.User).ToList();
+            return View(allTransaction);
+        }
     }
 }

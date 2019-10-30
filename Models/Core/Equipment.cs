4815f63e0422e7adeaf4b7c60198d81d5e9561fa
DarkAce01 <quadrikazeem01@gmail.com>
2019-10-30 02:34:31 +0100

    bug fix

--- Models/Core/Equipment.cs
+++ Models/Core/Equipment.cs
@@ -16,9 +16,9 @@ namespace SportEquipWeb.Models.Core
         public string ApplicationUserId { get; set; }
         
         public virtual ApplicationUser Owner { get; set; }
-        [Display(Name = "Short Description")]
+        [Display(Name = "Intro")]
         public string ShortDescription { get; set; }
-        [Display(Name = "Long Description")]
+        [Display(Name = "Details")]
         public string LongDescription { get; set; }
         [Display(Name ="Is Available ?")]
         public bool IsAvaible { get; set; }
